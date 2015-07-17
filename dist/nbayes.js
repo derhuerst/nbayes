b246d2de9d69e416682d9e7d679d0c9e058721e0
Jannis R <mail@jannisr.de>
2015-07-17 16:21:08 +0200

    rebuild `dist/` files

--- dist/nbayes.js
+++ dist/nbayes.js
@@ -1,10 +1,74 @@
-// nbayes | Jannis R | v2.0.1 | https://github.com/derhuerst/nbayes
+// nbayes | Jannis R | v2.0.2 | https://github.com/derhuerst/nbayes
 
 
 
 
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.nbayes = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = {
+
+
+
+	init: function () {
+		this._i = {};
+		this.total = 0;
+
+		return this;
+	},
+
+
+
+	set: function (item, n) {
+		if (item === '') return this;
+
+		this._i[item] = n;
+
+		return this;
+	},
+
+	get: function (item) {
+		return this._i[item] || 0;
+	},
+
+	increase: function (item, n) {
+		if (item === '') return this;
+
+		if (!this._i[item])
+			this._i[item] = n;
+		else
+			this._i[item] += n;
+		this.total += n;
+
+		return this;
+	},
+
+
+
+	addBagOfWords: function (bagOfWords) {
+		var i;
+
+		for (i in bagOfWords._i) {
+			if (!bagOfWords._i.hasOwnProperty(i)) continue;
+			this.increase(i, bagOfWords.get(i));
+		}
+
+		return this;
+	},
+
+	addWords: function (words) {
+		var i, length;
+		for (i = 0, length = words.length; i < length; i++) {
+			this.increase(words[i], 1);
+		}
+
+		return this;
+	},
+
+
+
+};
+
+},{}],2:[function(require,module,exports){
 Vocabulary =	require('./Vocabulary.js')
 BagOfWords =	require('./BagOfWords.js')
 
@@ -120,71 +184,7 @@ module.exports = {
 
 };
 
-},{"./BagOfWords.js":2,"./Vocabulary.js":3}],2:[function(require,module,exports){
-module.exports = {
-
-
-
-	init: function () {
-		this._i = {};
-		this.total = 0;
-
-		return this;
-	},
-
-
-
-	set: function (item, n) {
-		if (item === '') return this;
-
-		this._i[item] = n;
-
-		return this;
-	},
-
-	get: function (item) {
-		return this._i[item] || 0;
-	},
-
-	increase: function (item, n) {
-		if (item === '') return this;
-
-		if (!this._i[item])
-			this._i[item] = n;
-		else
-			this._i[item] += n;
-		this.total += n;
-
-		return this;
-	},
-
-
-
-	addBagOfWords: function (bagOfWords) {
-		var i;
-
-		for (i in bagOfWords._i) {
-			if (!bagOfWords._i.hasOwnProperty(i)) continue;
-			this.increase(i, bagOfWords.get(i));
-		}
-
-		return this;
-	},
-
-	addWords: function (words) {
-		var i, length;
-		for (i = 0, length = words.length; i < length; i++) {
-			this.increase(words[i], 1);
-		}
-
-		return this;
-	},
-
-
-
-};
-
-},{}],3:[function(require,module,exports){
+},{"./BagOfWords.js":1,"./Vocabulary.js":3}],3:[function(require,module,exports){
 module.exports = {
 
 
@@ -230,5 +230,5 @@ module.exports = {
 
 };
 
-},{}]},{},[1])(1)
+},{}]},{},[2])(2)
 });
